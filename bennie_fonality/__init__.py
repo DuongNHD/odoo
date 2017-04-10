4e7e91654973a64305c42b5d7394e8cf71714c6a
BENNIE NGUYEN <danny_nguyen@qsoftvietnam.com>
2017-04-10 11:45:09 +0700

    delete

--- bennie_fonality/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from models import fonality
-from models import make_call
-from wizards import fonality
-from wizards import message_dialog
-from wizards import calling_dialog
-from wizards import make_call
